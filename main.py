ffdcdd8af13ad58c8861f85d60b090413bcaf930
György Végh <gyorgy.vegh.job@gmail.com>
2022-06-07 14:38:11 +0200

    Green

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-def main():
-    return False
+def dig_pow(n, p):
+    k=0
+    for i in range(len(str(n))):
+        k += f_computing(int(str(n)[i]), p)
+        p += 1
+    if k % n != 0:
+        k = -1
+    else:
+        k = k / n 
+        
+    return k
+
+def f_computing(digit, p):
+    return pow(digit, p)
